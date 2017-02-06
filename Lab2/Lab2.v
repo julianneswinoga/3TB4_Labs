0bc5e509a819e813b293f3f210febafbe7731f11
Julianne Swinoga <julianneswinoga@gmail.com>
2017-02-06 14:04:18 -0500

    All modules defined

--- Lab2/Lab2.v
+++ Lab2/Lab2.v
@@ -1,4 +1,53 @@
-module Lab2Prelab(
+module Lab2 (
+	input CLOCK_50,
+	input [2:0] KEY,
+	output wire [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7
+	);
+
+endmodule
+
+module control_ff (
+	input Clock, ff_in, Set, Clear,
+	output reg Q
+	);
+
+	always @ (posedge Clock & Set)
+		if(Clear) begin
+			Q <= 1;
+		end
+		else if (ff_in) begin
+			Q <= ~Q;
+		end
+endmodule
+
+module clock_divider (
+	input Clock, Reset,
+	output reg Pulse_ms
+	);
+
+	always @(posedge Clock)
+		if (Reset)
+			  Pulse_ms <= 0;
+		else
+			Pulse_ms <= ~Pulse_ms;
+endmodule
+
+module hex_counter (
+	input Clock, Reset, Enable, Stp,
+	output reg [31:0]Q
+	);
+	
+	always @(posedge Clock)
+		if (Reset) begin
+			Q <= 0;
+		end 
+		else if (Stp)begin end
+		else if (Enable) begin
+			Q <= Q + 1;
+		end
+endmodule
+
+module hex_to_bcd_converter(
 	input wire clk, Reset,
 	input [31:0] hex_number,
 	output reg [3:0] bcd_digit_0,bcd_digit_1,bcd_digit_2,bcd_digit_3,bcd_digit_4,bcd_digit_5, bcd_digit_6, bcd_digit_7
@@ -46,4 +95,22 @@ module Lab2Prelab(
 		bcd_digit_1 = shift[39:36];
 		bcd_digit_0 = shift[35:32];
    end
+endmodule
+
+module seven_seg_decoder (
+	input [3:0] x,
+	output[6:0] hex_LEDs
+	);
+	
+	reg [6:2] top_5_segments;
+
+	assign hex_LEDs[0] = 1'b1/* Insert expression for segment 0 here */;
+	assign hex_LEDs[1] = 1'b1/* Insert expression for segment 1 here */;
+
+	/*always at (*) begin
+	if (1)
+		hex_LEDs[2] = 0;
+	else
+		hex_LEDs[3] = 1;
+	end*/
 endmodule

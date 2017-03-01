37a8858b5c62adc7b305716021415445291e47db
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-01 13:18:19 -0500

    Added more files

--- Lab3/dsp_subsystem.v
+++ Lab3/dsp_subsystem.v
@@ -1,5 +1,10 @@
-module dsp_subsystem (input sample_clock,  input reset, input [1:0] selector, input [15:0] input_sample, output [15:0] output_sample);
+module dsp_subsystem (
+	input sample_clock, reset,
+	input [1:0] selector,
+	input [15:0] input_sample,
+	output [15:0] output_sample
+	);
 
-assign output_sample = input_sample;
+	assign output_sample = input_sample;
 
 endmodule

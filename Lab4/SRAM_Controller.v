85ed31875bb1323d7994660acefd176098b5fce8
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-16 21:59:42 -0400

    Starting lab 4

--- Lab4/SRAM_Controller.v
+++ Lab4/SRAM_Controller.v
@@ -1,39 +1,13 @@
-/*****************************************************************************
- *                                                                           *
- * Module:       SRAM_Controller                                             *
- * Description:                                                              *
- *      This module is used for the sram controller for 3TB4 lab 4           *
- *                                                                           *
- *****************************************************************************/
-
 module SRAM_Controller (
-input           clk,
-input				 reset_n,
-input		[17:0]	address,
-input				chipselect,
-input		[1:0]	byte_enable,
-input				read,
-input				write,
-input		[15:0]	write_data,
-
-// Bidirectionals
-inout		[15:0]	SRAM_DQ,
-
-// Outputs
-output		[15:0]	read_data,
-
-output		[17:0]	SRAM_ADDR,
-
-output				SRAM_CE_N,
-output				SRAM_WE_N,
-output				SRAM_OE_N,
-output				SRAM_UB_N,
-output				SRAM_LB_N
-);
-
-// Add your code here
-
-
+	input clk, reset_n, chipselect, read, write,
+	input	[17:0] address,
+	input	[1:0]	byte_enable,
+	input	[15:0] write_data,
+	inout	[15:0] SRAM_DQ,
+	output [15:0] read_data,
+	output [17:0] SRAM_ADDR,
+	output SRAM_CE_N, SRAM_WE_N, SRAM_OE_N, SRAM_UB_N,SRAM_LB_N			
+	);
 
 
 endmodule

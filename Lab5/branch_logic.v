403099c8785b0e5c21cbdbdb7a3666dfa210df8e
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-24 16:03:09 -0400

    branch_logic done

--- Lab5/branch_logic.v
+++ Lab5/branch_logic.v
@@ -1,5 +1,8 @@
-module branch_logic (input [7:0] register0, output branch);
+module branch_logic (
+	input [7:0] register0,
+	output branch
+	);
 
-
+	assign branch = (register0 == 7'b0);
 
 endmodule

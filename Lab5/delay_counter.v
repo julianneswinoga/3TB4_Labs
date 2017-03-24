3aa0fc568b9f84e664b1e698b02d8bf1476a2902
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-24 17:21:57 -0400

    WIP

--- Lab5/delay_counter.v
+++ Lab5/delay_counter.v
@@ -1,5 +1,29 @@
-module delay_counter (input clk, reset_n, start, enable, input [7:0] delay, output done);
-parameter BASIC_PERIOD=19'd500000;
-
+module delay_counter (
+	input clk, start, enable,
+	input [7:0] delay,
+	output reg done
+	);
+	
+	parameter CLK_HZ = 500000;
+	parameter DELAY_HZ = 100;
+	
+	reg [7:0] delay_register, ms_counter;
+	
+	always @ (posedge clk) begin
+		if (start)
+			delay_register <= delay;
+			ms_counter <= 0;
+		if (enable) begin
+			if (delay_register == 7'b0)
+				done <= 1;
+			else
+				done <= 0;
+			if (ms_counter == 7'b0) begin
+				ms_counter <= CLK_HZ / DELAY_HZ;
+				delay_register <= delay_register - 1;
+			end else
+				ms_counter <= ms_counter - 1;
+		end
+	end
 
 endmodule

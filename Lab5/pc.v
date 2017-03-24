382144073f4b82bfef23f156357f990001ec399d
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-24 15:03:47 -0400

    pc done

--- Lab5/pc.v
+++ Lab5/pc.v
@@ -1,7 +1,15 @@
-module pc (input clk, reset_n, branch, increment, input [7:0] newpc,
-			output reg [7:0] pc);
-parameter RESET_LOCATION = 8'h00;
-
-
-			
+module pc(
+	input clock, reset_n, branch, increment,
+	input [7:0] newpc,
+	output reg [7:0] pc
+	);
+	
+	always @ (posedge clock) begin
+		if (!reset_n)
+			pc = 8'h00;
+		else if (branch)
+			pc = newpc;
+		else if (increment)
+			pc = pc + 1;
+	end
 endmodule

8ed993d54651869f0a89535b0fc03bb7047d6399
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-24 15:35:24 -0400

    alu done

--- Lab5/alu.v
+++ Lab5/alu.v
@@ -1,5 +1,18 @@
-module alu (input add_sub, set_low, set_high, input [7:0] operanda , operandb, output reg [7:0] result);
-
-
-
+module alu(
+	input add_sub, set_low, set_high,
+	input [7:0] operanda, operandb,
+	output reg [7:0] result
+	);
+	
+	always @(*) begin
+		if (set_low)
+			result = {operanda[7:4], operandb[3:0]};
+		else if (set_high)
+			result = {operandb[3:0], operanda[3:0]};
+		else if (add_sub)
+			result = operanda - operandb;
+		else if (!add_sub)
+			result = operanda + operandb;
+	end
+	
 endmodule

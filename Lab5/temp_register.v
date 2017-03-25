62658c12db63651adecddddb841bfc2516ecf1ec
Julianne Swinoga <julianneswinoga@gmail.com>
2017-03-25 01:24:05 -0400

    temp register done

--- Lab5/temp_register.v
+++ Lab5/temp_register.v
@@ -1,6 +1,21 @@
-module temp_register (input clk, reset_n, load, increment, decrement, input [7:0] data,
-					output negative, positive, zero);
+module temp_register (
+	input clk, reset_n, load, increment, decrement,
+	input [7:0] data,
+	output reg negative, positive, zero
+	);
 
-
+	reg [7:0] internal_register;
+	
+	always @ (posedge clk) begin
+		if (load)
+			internal_register = data;
+		if (increment)
+			internal_register = internal_register + 1;
+		if (decrement)
+			internal_register = internal_register - 1;
+		zero = (internal_register == 7'b0);
+		positive = (internal_register[7] == 0);
+		negative = (internal_register[7] == 1);
+	end
 
 endmodule
